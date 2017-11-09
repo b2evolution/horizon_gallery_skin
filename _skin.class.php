ab7d9e706b347bc653f42816f4f82d5de334a1e0
yurabakhtin <yurybakh@gmail.com>
2017-11-09 17:48:32 +0300

    Update version to 7

--- _skin.class.php
+++ _skin.class.php
@@ -17,7 +17,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class horizon_gallery_Skin extends Skin
 {
-	var $version = '1.0';
+	var $version = '7.0';
 	
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
